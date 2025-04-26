0a1d9ae5ef4f58181cabaf7ea5f9eabdc130c99a
Matheus-Merlos <matheusazrmerlos@gmail.com>
2025-04-25 22:14:27 -0300

    Adicionei um workflow test (depois adiciono o resto)

--- drizzle/0001_flat_patch.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE "course-tag" RENAME TO "course_tag";--> statement-breakpoint
-ALTER TABLE "course_tag" DROP CONSTRAINT "course-tag_course_course_id_fk";
---> statement-breakpoint
-ALTER TABLE "course_tag" ADD CONSTRAINT "course_tag_course_course_id_fk" FOREIGN KEY ("course") REFERENCES "public"."course"("id") ON DELETE cascade ON UPDATE cascade;
