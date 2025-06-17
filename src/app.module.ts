1e1f4e4aca64379f8573869d851d01821f81ceb4
Matheus-Merlos <matheusazrmerlos@gmail.com>
2025-06-17 14:28:07 -0300

    feat: remover service desnecessário

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ExamModule } from './exam/exam.module';
 import { UserModule } from './user/user.module';
 @Module({
   imports: [ExamModule, UserModule, ExamModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
