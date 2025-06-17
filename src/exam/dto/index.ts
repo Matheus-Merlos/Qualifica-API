f0e3a7bfbbb91663a629b36db010974bcd858b05
Matheus-Merlos <matheusazrmerlos@gmail.com>
2025-06-17 14:32:51 -0300

    feat: refatoração da organização de DTOs, e remoção de código inutilizado

--- src/exam/dto/index.ts
+++
@@ -1,6 +0,0 @@
-import { AlternativeDto } from './alternative.dto';
-import { CreateExamDto } from './create-exam.dto';
-import { QuestionDto } from './question.dto';
-import { UpdateExamDto } from './update-exam.dto';
-
-export { AlternativeDto, CreateExamDto, QuestionDto, UpdateExamDto };
--- src/exam/exam.controller.ts
+++ src/exam/exam.controller.ts
@@ -6,22 +6,18 @@ import {
   Param,
   Patch,
   Post,
-  UseGuards,
 } from '@nestjs/common';
-import { AuthGuard } from 'src/user/auth/auth.guard';
-import { CreateExamDto } from './dto/create-exam.dto';
-import { UpdateExamDto } from './dto/update-exam.dto';
+import { CreateExamDTO, UpdateExamDTO } from './exam.dto';
 import { ExamService } from './exam.service';
 
 @Controller('exam/:sectionId')
-@UseGuards(AuthGuard)
 export class ExamController {
   constructor(private readonly examService: ExamService) {}
 
   @Post()
   create(
     @Param('sectionId') sectionId: string,
-    @Body() createExamDto: CreateExamDto,
+    @Body() createExamDto: CreateExamDTO,
   ) {
     return this.examService.create(+sectionId, createExamDto);
   }
@@ -43,7 +39,7 @@ export class ExamController {
   update(
     @Param('sectionId') sectionId: string,
     @Param('examId') examId: string,
-    @Body() updateExamDto: UpdateExamDto,
+    @Body() updateExamDto: UpdateExamDTO,
   ) {
     return this.examService.update(+sectionId, +examId, updateExamDto);
   }
