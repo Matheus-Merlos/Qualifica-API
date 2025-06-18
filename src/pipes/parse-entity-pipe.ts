394278f6c51449e342661c7467b523f6a3d52db3
Matheus-Merlos <matheusazrmerlos@gmail.com>
2025-06-18 16:10:24 -0300

    Criar um pipe genérico para evitar repetição de código

---
+++ src/pipes/parse-entity-pipe.ts
@@ -0,0 +1,34 @@
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  NotFoundException,
+  PipeTransform,
+} from '@nestjs/common';
+import { Column, eq, getTableName } from 'drizzle-orm';
+import { PgTable } from 'drizzle-orm/pg-core';
+import db from 'src/db';
+
+export abstract class BaseParseEntityPipe implements PipeTransform {
+  protected abstract table: PgTable & { id: Column<any> };
+
+  async transform(value: any, metadata: ArgumentMetadata) {
+    if (typeof value !== 'number') {
+      throw new BadRequestException(
+        `${metadata.metatype?.name} must be a number!`,
+      );
+    }
+
+    const [entity] = await db
+      .select()
+      .from(this.table as PgTable)
+      .where(eq(this.table.id, value));
+
+    if (!entity) {
+      throw new NotFoundException(
+        `${getTableName(this.table)} with id ${value} does not exists.`,
+      );
+    }
+
+    return value;
+  }
+}
--- src/pipes/parse-user.pipe.ts
+++ src/pipes/parse-user.pipe.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { exam } from 'src/db/schema';
+import { user } from 'src/db/schema';
 import { BaseParseEntityPipe } from './parse-entity-pipe';
 
 @Injectable()
-export class ParseExamPipe extends BaseParseEntityPipe {
-  protected table = exam;
+export class ParseUserPipe extends BaseParseEntityPipe {
+  protected table = user;
 }
