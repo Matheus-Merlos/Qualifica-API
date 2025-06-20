c634819fa8edfb4d9aa703bae1359e978478ef08
Matheus-Merlos <matheusazrmerlos@gmail.com>
2025-06-20 17:28:53 -0300

    feat: criação de um interceptor para ficar lançando internalservererror e printando na tela para debug

---
+++ src/interceptors/error.interceptor.ts
@@ -0,0 +1,26 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  InternalServerErrorException,
+  NestInterceptor,
+} from '@nestjs/common';
+import { catchError, Observable, throwError } from 'rxjs';
+
+@Injectable()
+export class ErrorInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      catchError((error) => {
+        console.error(error);
+
+        return throwError(
+          () =>
+            new InternalServerErrorException(
+              `The server encountered an error: ${(error as Error).message}`,
+            ),
+        );
+      }),
+    );
+  }
+}
