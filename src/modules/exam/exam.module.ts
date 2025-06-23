d4e72f5b3c0b09d992dc3a3180e0d7454b77775b
Matheus-Merlos <matheusazrmerlos@gmail.com>
2025-06-23 19:01:22 -0300

    feat: criação do controller, service e module de subscription

--- src/modules/exam/exam.module.ts
+++ src/modules/exam/exam.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ExamService } from './exam.service';
 import { ExamController } from './exam.controller';
-import { AnswersModule } from './answers/answers.module';
+import { ExamService } from './exam.service';
 
 @Module({
   controllers: [ExamController],
   providers: [ExamService],
-  imports: [AnswersModule],
 })
 export class ExamModule {}
---
+++ src/modules/subscription/subscription.service.ts
@@ -0,0 +1,6 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class SubscriptionService {
+  async subscribe(userId: number, courseId: number) {}
+}
