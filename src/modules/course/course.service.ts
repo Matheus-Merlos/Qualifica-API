e3cdc818f8f40ca35fe2e8779a335233548486c1
Matheus-Merlos <matheusazrmerlos@gmail.com>
2025-06-25 13:07:47 -0300

    feat: adicionar tags nas pesquisas e em curso individual

--- src/modules/course/course.service.ts
+++ src/modules/course/course.service.ts
@@ -18,7 +18,7 @@ import { CreateCourseDTO, PatchCourseDTO } from './course.dto';
 @Injectable()
 export class CourseService {
   async search(searchParam: string = '') {
-    return await db
+    const courses = await db
       .select({
         id: course.id,
         name: course.name,
@@ -28,6 +28,27 @@ export class CourseService {
       .where(ilike(course.name, `%${searchParam.replaceAll('-', ' ')}%`))
       .orderBy(asc(course.name))
       .limit(25);
+
+    let index = 0;
+    for (const crs of courses) {
+      const tags: Array<unknown> = [];
+      const courseWithTags = { ...crs, tags };
+
+      const dbTags = await db
+        .select()
+        .from(courseTag)
+        .where(eq(courseTag.course, crs.id));
+
+      const tagNames = dbTags.map((tag) => tag.tag);
+
+      courseWithTags.tags = tagNames;
+
+      courses[index] = courseWithTags;
+
+      index++;
+    }
+
+    return courses;
   }
 
   async retrieve(courseId: number) {
@@ -107,6 +128,15 @@ export class CourseService {
       courseWithSections.sections[index] = sectionWithResources;
       index++;
     }
+
+    const dbTags = await db
+      .select()
+      .from(courseTag)
+      .where(eq(courseTag.course, courseWithSections.id));
+
+    const tagNames = dbTags.map((tag) => tag.tag);
+
+    courseWithSections['tags'] = tagNames;
 
     return courseWithSections;
   }
